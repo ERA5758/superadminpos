c873a5e10e38551deabe2119e68ffbcf897625e7
TAKIS MALAM <de.madjor.ryo@gmail.com>
2025-10-15 15:32:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "kasir-pos-chika-toko-354-1abce",
+  "appId": "1:582069956448:web:4c901ec70a1afe08837805",
+  "apiKey": "AIzaSyAT7g9niddpksmzpOhcz6g8dtWnq5Vd6ms",
+  "authDomain": "kasir-pos-chika-toko-354-1abce.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "582069956448"
+};
