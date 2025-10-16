91040491fde665b8d70495e1dfdc205f13123037
TAKIS MALAM <de.madjor.ryo@gmail.com>
2025-10-16 06:42:32 +0000

    update superadmin

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-import type {NextConfig} from 'next';
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -32,4 +36,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
