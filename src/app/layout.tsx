3cb3d98c37235f4ac51729954a7f2d3ad7f8765e
TAKIS MALAM <de.madjor.ryo@gmail.com>
2025-10-18 13:58:48 +0000

    Pwa nya tolong diperbaiki untuk icon nya. Buat icon rumah aja

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ export default function RootLayout({
     <html lang="id">
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
